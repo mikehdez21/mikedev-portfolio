e588bc98b21b8c4ca3161fffbbc3381c08775320
mikehdez21 <mikehdez21@hotmail.com>
2026-09-08 13:15:17 -0600

    init project - astro - cloudflare pages

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    },
+
+    imageService: "cloudflare"
+  }),
+  vite: {
+    optimizeDeps: {
+      include: ['astro/assets/services/noop', 'astro/logger/json']
+    }
+  }
+});
